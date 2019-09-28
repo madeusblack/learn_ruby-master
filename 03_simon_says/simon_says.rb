65500b934b4435aa5f5f97dd1fb11e85f4629f19
Yayson Olivares <44871312+madeusblack@users.noreply.github.com>
2019-09-28 01:50:57 -0300

    edited simon says
    edited simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,29 @@
 #write your code here
+def echo word
+    result=word
+end
+def shout word
+    result=word.upcase
+end
+def repeat (word,times=2) 
+    todo=0
+    result=""
+    while todo<times
+
+        result=result + word + " "
+        todo=todo+1
+    end
+    result=result.chop
+    result
+end
+def start_of_word (word,amount)
+    times=0
+    result=""
+    index=0
+    while times < amount
+    result =result + word[index]
+    index=index+1
+    times=times+1
+    end
+    result
+end
